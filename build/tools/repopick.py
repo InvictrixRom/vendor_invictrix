LineageOS/android_vendor_cm

repopick: use https by default for repopicking
Change-Id: Iae1fde3515b0377a2b9253102c82d28b0fdaca4a

build/tools/repopick.py
@@ -123,7 +123,7 @@ def fetch_query(remote_url, query):
 
 if __name__ == '__main__':
     # Default to LineageOS Gerrit
-    default_gerrit = 'http://review.lineageos.org'
+    default_gerrit = 'https://review.lineageos.org'
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''\
         repopick.py is a utility to simplify the process of cherry picking

InvictrixRom/vendor_invictrix

Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

build/tools/repopick.py
@@ -143,7 +143,7 @@ def fetch_query(remote_url, query):
 
 if __name__ == '__main__':
     # Default to Invictrix Gerrit
-    default_gerrit = 'http://review.invictrixrom.com'
+    default_gerrit = 'https://review.invictrixrom.com'
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''\
         repopick.py is a utility to simplify the process of cherry picking