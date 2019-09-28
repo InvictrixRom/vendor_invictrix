8a247b31108bb2dfbe17db8634dad0377671f66a
me2151 <dillon@invictrix.dev>
2019-09-28 16:35:20 -0400

    add fajita
    Change-Id: I1aa158c503bd0fc9bf8278adcd9fce3ae34e6970

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,10 @@
 supported_devices=(
   'marlin'
+  'fajita'
 )
 
 for device in ${supported_devices[@]}; do
     add_lunch_combo invictrix_${device}-user
     add_lunch_combo invictrix_${device}-userdebug
+    add_lunch_combo invictrix_${device}-eng
 done
